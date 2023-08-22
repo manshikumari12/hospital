54545b4fa1db182c5e2d295c4a7c744a59e707b2
Manshi Kumari <119393324+manshikumari12@users.noreply.github.com>
2023-08-22 19:06:19 +0530

    ok

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 require("dotenv").config()
 const connection = mongoose.connect("mongodb+srv://manshisbp:manshi@cluster0.uwpiapl.mongodb.net/hospital?retryWrites=true&w=majority");
-// /hhh
+// /hhhiljdfilsd
 module.exports = {connection}
